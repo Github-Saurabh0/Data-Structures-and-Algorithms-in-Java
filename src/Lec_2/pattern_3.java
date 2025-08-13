06640dcaf46a50b36ab6c42a7cf41dbd0982fc30
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-13 18:15:07 +0530

    Lec-2 Pattern-3

---
+++ src/Lec_2/pattern_3.java
@@ -0,0 +1,26 @@
+package Lec_2;
+
+import java.util.Scanner;
+
+public class pattern_3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Value: ");
+		int n = sc.nextInt();
+		int row = 1;
+		int star = n;
+		
+		while(row<=n) {
+			int i = 1;
+			while(i <= star){
+				System.out.print("* ");
+				i++;
+			}
+			row++;
+			star--;
+			System.out.println();
+		}
+		sc.close();
+	}
+}
