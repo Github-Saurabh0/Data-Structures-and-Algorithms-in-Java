429e75adb1b154b7f8a6f3976d7b5e11385ad015
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-13 18:11:16 +0530

    Lec-1 Coding-part-2

---
+++ src/Lec_1/code2_Arithmetic_Operation.java
@@ -0,0 +1,34 @@
+package Lec_1;
+
+public class code2_Arithmetic_Operation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("========================================");
+		int a = 7;
+		System.out.println("Value a = 7");
+		int b = 9;
+		System.out.println("Value b = 9");
+		int c = a + b;
+		System.out.println("========================================");
+		System.out.println("a + b is " + c);
+		System.out.println("========================================");
+		c = a - b;
+		System.out.println("========================================");
+		System.out.println("a - b is " + c);
+		System.out.println("========================================");
+		c = a * b;
+		System.out.println("========================================");
+		System.out.println("a * b is " + c);
+		System.out.println("========================================");
+		c =  a / b;
+		System.out.println("========================================");
+		System.out.println("a / b is " + c);
+		System.out.println("========================================");
+		c =  a % b;
+		System.out.println("========================================");
+		System.out.println("a % b is " + c);
+		System.out.println("========================================");
+	}
+
+}
