7ef8bf4b5c602afd2e8f2b21a6aa52a9c8831907
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-15 17:07:39 +0530

    Lec-2 Pattern-8

---
+++ src/Lec_2/pattern_8.java
@@ -0,0 +1,41 @@
+package Lec_2;
+
+import java.util.Scanner;
+
+public class pattern_8 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Value: ");
+		int n = sc.nextInt();
+		int row = 1;
+		int star = 1;
+		int space = n-1;
+		
+		while(row<=n) {
+			
+			//space
+			int i = 1;
+			while(i<=space) {
+				System.out.print("  ");
+				i++;
+				
+			}
+			
+			//star
+			int j = 1;
+			while(j<=star) {
+				System.err.print("* ");
+				j++;
+			}
+			
+			//next row prep..
+			row++;
+			System.out.println();
+			space--;
+			star+=2; // star = star + 2 
+			
+		}
+		sc.close();
+	}
+}
