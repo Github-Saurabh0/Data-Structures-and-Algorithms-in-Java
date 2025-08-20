8e16cbb49cd299c6639109fb857c71e6d92b5cd3
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-20 12:03:53 +0530

    Lec-3 Mirror-Pattern-10

---
+++ src/Lec_3/mirror_pattern_10.java
@@ -0,0 +1,70 @@
+package Lec_3;
+
+import java.util.Scanner;
+
+public class mirror_pattern_10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Value: ");
+		int n = sc.nextInt();
+		int row = 1;
+		int spacestart = 0;
+		int spacemiddle = n-2;
+
+		while (row <= n) {
+
+			
+			// space 
+			int i = 1; 
+			while(i<=spacestart)
+			{
+			 System.out.print("  ");
+			 i++;
+			 
+			}
+			
+			// star 
+			System.out.print("* ");
+				
+		    // space 
+			int k = 1; 
+			while(k<=spacemiddle)
+			{
+			 System.out.print("  ");
+				k++;
+			}
+			
+			// star	
+			if(row==n/2+1)
+			{
+				System.out.print("  ");
+			}
+			else
+			{
+				System.out.print("* ");
+			}
+			
+			
+			//next prep // Mirror Concept 
+			
+			if(row<n/2+1)
+			{
+				spacestart++;
+				spacemiddle-=2;
+				
+			}
+			else
+			{
+				spacestart--;
+				spacemiddle+=2;
+				
+			}
+			row++;
+			System.out.println();
+		}
+		sc.close();
+	}
+}
+
+
