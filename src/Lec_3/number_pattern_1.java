437eccaf1fd0b3cf5ad6e1b27fb23be36f5e36bb
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-21 16:16:49 +0530

    Lec-3 Number-Pattern-1

---
+++ src/Lec_3/number_pattern_1.java
@@ -0,0 +1,43 @@
+package Lec_3;
+
+import java.util.Scanner;
+
+public class number_pattern_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Value: ");
+		int n = sc.nextInt();
+		int row = 1;
+		int star = 1;
+		int space = n-1;
+		
+		while(row<=n) {
+			
+			//space
+			int i = 1;
+			while(i<=space) {
+				System.out.print("  ");
+				i++;
+				
+			}
+			
+			//star
+			int j = 1;
+			while(j<=star) {
+				System.out.print("1 " );
+				j++;
+			}
+			
+			//next row prep..
+			row++;
+			System.out.println();
+			space--;
+			star+=2; // star = star + 2 
+			
+		}
+		sc.close();
+
+	}
+
+}
