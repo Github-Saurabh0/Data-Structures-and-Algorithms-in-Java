3ac2ae2e361fe000f46369f4d2968f60a66ae41d
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-23 16:00:51 +0530

    Lec-4 for_loop

---
+++ src/Lec_4/for_loop.java
@@ -0,0 +1,10 @@
+package Lec_4;
+
+public class for_loop {
+	public static void main(String[] args) {
+		// This loop prints numbers from 1 to 5
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Current number: " + i);
+		}
+	}
+}
