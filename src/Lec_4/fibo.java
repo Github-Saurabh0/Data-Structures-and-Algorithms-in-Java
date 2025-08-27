fbd023402bd15c2972ef62b7dedbf5c8b30cc73a
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-27 18:35:06 +0530

    Lec-4 fibo

---
+++ src/Lec_4/fibo.java
@@ -0,0 +1,26 @@
+package Lec_4;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class fibo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Value: ");
+		int n = sc.nextInt();
+		
+		int a =0;
+		int b =1;
+		
+		for (int i = 1 ; i <=n ; i++)
+		{
+			int c = a + b;
+			a =b;
+			b =c;
+		}
+		System.out.println(a);
+		
+	}
+
+}
