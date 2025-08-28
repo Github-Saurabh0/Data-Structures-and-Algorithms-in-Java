1e7e9f64dcee84aa2d990c5eee490053fb92bd4a
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-08-28 16:55:25 +0530

    Lec-4 Gcd_Hcf

---
+++ src/Lec_4/Gcd_Hcf.java
@@ -0,0 +1,27 @@
+package Lec_4;
+
+import java.util.Scanner;
+
+public class Gcd_Hcf {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Divisor: ");
+		int divisor = sc.nextInt();
+		// Divisor = 36
+		System.out.print("Enter Divident: ");
+		int divident = sc.nextInt();
+		// Divident = 60
+		
+		while(divident % divisor != 0)
+		{
+			int reminder = divident % divisor;
+			divident = divisor;
+			divisor = reminder;
+			
+		}
+		
+		System.out.println("GCD is: "+ divisor);
+	}
+
+}
