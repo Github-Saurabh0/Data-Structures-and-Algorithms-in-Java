1a350fb1722bde1443c340a6b663a22904e45bbb
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-09-29 18:48:41 +0530

    Lec-6 Data Type Casting => without error, syste, ne esa kya kiya, jisse error nhi aya..

---
+++ src/Lec_6/data_type_2.java
@@ -0,0 +1,21 @@
+package Lec_6;
+
+public class data_type_2 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int x1 = - 5;
+		int i = 428;
+		byte b = 10;
+		long l = 10;
+		short s = 10;
+		b = (byte)(i);		
+		System.out.println(b);
+		int a = 90;
+		byte b1 = 7;
+		a = b1;
+		System.out.println(a);
+
+	}
+
+}
